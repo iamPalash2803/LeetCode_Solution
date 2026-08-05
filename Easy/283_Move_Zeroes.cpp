c4af790134b28b70ce7b42e0c2459934ec32126d
Palash Mondal <palash012006@gmail.com>
2026-08-05 19:45:08 +0530

    Add solution: Move Zeroes (Easy)

--- Easy/283_Move_Zeroes.cpp
+++ Easy/283_Move_Zeroes.cpp
@@ -34,18 +34,26 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int n = nums.size();
-        vector<int> temp;
-        for(int i = 0; i<n; i++){
-            if(nums[i] != 0){
-                temp.push_back(nums[i]);
+        int j = -1;
+
+        //Find te first zero inside the array
+        for(int i =0; i<n; i++){
+            if(nums[i] == 0){
+                j =i;
+                break;
             }
         }
-        int nz = temp.size();
-        for(int i = 0; i<nz; i++){
-            nums[i] = temp[i];
+
+        //indicates there is nozero inside the array
+        if(j == -1){
+            return;
         }
-        for(int i = nz; i<n; i++){
-            nums[i] = 0;
+
+        for(int i = j+1; i<n; i++){
+            if(nums[i] != 0){
+                swap(nums[i] , nums[j]);
+                j++;
+            }
         }
     }
 };
