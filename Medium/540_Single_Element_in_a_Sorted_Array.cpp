5529611d0deb563b3cd468a32b7878343023e277
Palash Mondal <palash012006@gmail.com>
2026-08-07 18:11:40 +0530

    Add solution: Single Element in a Sorted Array (Medium)

--- Medium/540_Single_Element_in_a_Sorted_Array.cpp
+++ Medium/540_Single_Element_in_a_Sorted_Array.cpp
@@ -31,10 +31,40 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int ans = 0;
-        for(int i=0; i<nums.size(); i++){
-            ans ^=nums[i];
+        int n = nums.size();
+        int st=0, end = n;
+        //only one element inside array
+        if(n==1) return nums[0];
+
+        while(st <= end){
+            int mid = st + (end - st)/2;
+
+            if(mid == 0 && nums[mid] != nums[mid+1])
+                return nums[mid];
+            if(mid == n-1 && nums[mid] != nums[mid-2])
+                return nums[mid];
+
+            if(nums[mid-1] != nums[mid] && nums[mid] !=nums[mid+1]){
+                return nums[mid];
+            }
+
+            if(mid % 2 == 0){
+                if(nums[mid] == nums[mid-1]){
+                    end = mid-1;
+                }
+                else{
+                    st = mid+1;
+                }
+            }
+            else{
+                if(nums[mid] == nums[mid-1]){
+                    st = mid+1;
+                }
+                else{
+                    end = mid-1;
+                }
+            }
         }
-        return ans;
+        return -1;
     }
 };
