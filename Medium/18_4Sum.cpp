31eb58f02d2aa97acf6cd7eb8bd7b24c3e5dcf96
Palash Mondal <palash012006@gmail.com>
2026-08-16 13:24:59 +0530

    Add solution: 4Sum (Medium)

--- Medium/18_4Sum.cpp
+++ Medium/18_4Sum.cpp
@@ -41,45 +41,45 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-
-        int n = nums.size();
+        int n  = nums.size();
         vector<vector<int>> ans;
 
         sort(nums.begin(), nums.end());
 
-        for (int i = 0; i < n; i++)
-        {
+        for(int i =0; i<n; i++){
+
             //Skip the first duplicate element if present
-            if (i > 0 && nums[i] == nums[i - 1])
+            if(i>0 && nums[i] == nums[i-1])
                 continue;
 
-            for (int j = i + 1; j < n; j++)
-            {
+            for(int j = i+1; j<n; j++){
+
                 // Skip duplicate second elements.
-                if (j > i + 1 && nums[j] == nums[j - 1])
+                if(j>i+1 && nums[j] == nums[j-1])
                     continue;
 
-                int p = j + 1, q = n - 1;
-                while (p < q)
-                {
+                int p = j+1, q = n-1;
+                while(p < q ){
                     long long sum = (long long)nums[i] + (long long)nums[j] + (long long)nums[p] + (long long)nums[q];
 
-                    if (sum < target)
+                    if(sum < target)
                         p++;
-                    else if (sum > target)
+
+                    else if(sum > target)
                         q--;
-                    else
-                    {
+
+                    else{
                         ans.push_back({nums[i], nums[j], nums[p], nums[q]});
-                        p++;
-                        q--;
+                        p++; q--;
+
                         // Skip duplicate values of p
-                        while (p < q && nums[p] == nums[p - 1])
+                        while(p <q && nums[p] == nums[p-1])
                             p++;
                     }
                 }
             }
         }
+
         return ans;
         
     }
