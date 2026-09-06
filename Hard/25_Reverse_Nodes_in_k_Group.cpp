a985994db0beabac64fab545c1214d349cbda695
Palash Mondal <palash012006@gmail.com>
2026-09-06 11:11:15 +0530

    Add solution: Reverse Nodes in k-Group (Hard)

---
+++ Hard/25_Reverse_Nodes_in_k_Group.cpp
@@ -0,0 +1,84 @@
+/*
+ * Problem #25: Reverse Nodes in k-Group
+ * Difficulty: Hard
+ *
+ * ----- Description -----
+ *
+ * Given the head of a linked list, reverse the nodes of the list k at a time, and return the modified list.
+ * 
+ * k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of k then left-out nodes, in the end, should remain as it is.
+ * 
+ * You may not alter the values in the list's nodes, only nodes themselves may be changed.
+ * 
+ *  
+ * Example 1:
+ * 
+ * Input: head = [1,2,3,4,5], k = 2
+ * Output: [2,1,4,3,5]
+ * 
+ * 
+ * Example 2:
+ * 
+ * Input: head = [1,2,3,4,5], k = 3
+ * Output: [3,2,1,4,5]
+ * 
+ * 
+ *  
+ * Constraints:
+ * 
+ * 
+ * 	The number of nodes in the list is n.
+ * 	1 <= k <= n <= 5000
+ * 	0 <= Node.val <= 1000
+ * 
+ * 
+ *  
+ * Follow-up: Can you solve the problem in O(1) extra memory space?
+ *
+ * ----- Solution -----
+ */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode* temp = head;
+        int count = 0;
+
+        //Cheak if k nodes exists or not
+        while(count < k){
+            if(temp == NULL){
+                return head;
+            }
+            temp = temp->next;
+            count++;
+        }
+
+        //Recursive call for rest of the linked list
+        ListNode* prevNode = reverseKGroup( temp, k);
+
+        //reverse current group
+        temp = head; count = 0;
+        while(count < k){
+            ListNode* next = temp->next;
+            temp->next = prevNode;
+
+            prevNode = temp;
+            temp = next;
+
+            count++;
+        }
+
+        return prevNode;
+
+    }
+};
